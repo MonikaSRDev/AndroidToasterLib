c4154c3bf4517e05ee75e1b0d7dc1df4371ff6e2
MonikaSRDev <monikasr0929@gmail.com>
2023-03-02 17:43:36 +0530

    Initial Commit

---
+++ toasterlibrary/src/main/java/com/example/toasterlibrary/ToastMessage.kt
@@ -0,0 +1,13 @@
+package com.example.toasterlibrary
+
+import android.content.Context
+import android.widget.Toast
+
+class ToastMessage {
+
+    companion object{
+        fun show(context : Context, message : String){
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
